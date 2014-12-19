e7834e0c5c66e8028a918fb8ab6b43261882096f
Joel Dare <joel@Joels-MacBook-Pro-2.local>
2014-12-19 11:00:00 -0700

    First commit.

---
+++ js/resources.js
@@ -0,0 +1,33 @@
+game.resources = [
+
+    /* Graphics.
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    // Our level tileset
+    {name: "area01_level_tiles", type: "image", src: "data/img/map/area01_level_tiles.png"},
+    // The man player spritesheet
+    {name: "gripe_run_right", type: "image", src: "data/img/sprite/gripe_run_right.png"},
+
+    /* Texture Atlases
+     * @example
+     * {name: "texture", type: "json", src: "data/img/example_tps.json"},
+     */
+
+    /* Maps.
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "area01", type: "tmx", src: "data/map/area01.json"}
+
+    /* Background music.
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects.
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
+];
